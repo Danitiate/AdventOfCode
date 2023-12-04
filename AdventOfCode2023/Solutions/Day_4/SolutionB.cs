91e08671013fff7c8169ef391aaba53f185ad8ec
Daniel Dutta <daniel.e.dutta@gmail.com>
2023-12-04 11:21:40 +0100

    - Added solution to Day 4 Part 2

--- AdventOfCode2023/Solutions/Day_4/SolutionB.cs
+++ AdventOfCode2023/Solutions/Day_4/SolutionB.cs
@@ -1,6 +1,8 @@
 ﻿using AdventOfCode2023.Models;
 using AdventOfCode2023.Services;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace AdventOfCode2023.Solutions.Day_4
 {
@@ -24,8 +26,43 @@ namespace AdventOfCode2023.Solutions.Day_4
 
         public int GetAmountOfScratchCards(List<string> scratchCards)
         {
-            var sum = 0;
-            return sum;
+            var scratchCardsWonAmountDictionary = PopulateScratchCardsDictionary(scratchCards);
+            var totalAmountOfCopies = scratchCardsWonAmountDictionary.ToDictionary(kv => kv.Key, kv => 0);
+            for (var i = 1; i <= scratchCardsWonAmountDictionary.Count(); i++)
+            {
+                var amountOfCopies = totalAmountOfCopies[i];
+                totalAmountOfCopies[i]++;
+                var amountOfScratchCardsWon = scratchCardsWonAmountDictionary[i];
+                var maxRange = Math.Min(i + amountOfScratchCardsWon, scratchCardsWonAmountDictionary.Count);
+                for (var j = i + 1; j <= maxRange; j++)
+                {
+                    totalAmountOfCopies[j] += amountOfCopies + 1;
+                }
+            }
+
+            return totalAmountOfCopies.Sum(kv => kv.Value);
+        }
+
+        private Dictionary<int, int> PopulateScratchCardsDictionary(List<string> scratchCards)
+        {
+            var scratchCardsWonAmountDictionary = new Dictionary<int, int>();
+            for (int currentScratchCardNumber = 1; currentScratchCardNumber <= scratchCards.Count; currentScratchCardNumber++)
+            {
+                var scratchCard = scratchCards[currentScratchCardNumber - 1];
+                var numbers = scratchCard.Substring(scratchCard.IndexOf(":") + 1).Split("|");
+                var winningNumbers = numbers[0].Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                var currentNumbers = numbers[1].Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                var matchingNumbers = GetMatchingNumbers(winningNumbers, currentNumbers);
+                var wonScratchCards = scratchCards.GetRange(currentScratchCardNumber, matchingNumbers.Count);
+                scratchCardsWonAmountDictionary.Add(currentScratchCardNumber, wonScratchCards.Count);
+            }
+
+            return scratchCardsWonAmountDictionary;
+        }
+
+        private List<string> GetMatchingNumbers(string[] winningNumbers, string[] currentNumbers)
+        {
+            return currentNumbers.Where(n => winningNumbers.Contains(n)).ToList();
         }
     }
 }
