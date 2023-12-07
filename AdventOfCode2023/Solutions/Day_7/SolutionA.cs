947519e1868c3a1e892968d1901032d9f487b6e0
Daniel Dutta <daniel.e.dutta@gmail.com>
2023-12-07 09:38:57 +0100

    - Added solution for Day 7 Part 1 - Added task description for Day 7 Part 2

--- AdventOfCode2023/Solutions/Day_7/SolutionA.cs
+++ AdventOfCode2023/Solutions/Day_7/SolutionA.cs
@@ -2,6 +2,7 @@
 using AdventOfCode2023.Services;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace AdventOfCode2023.Solutions.Day_7
 {
@@ -22,14 +23,210 @@ namespace AdventOfCode2023.Solutions.Day_7
         public void Solve()
         {
             var stringInputs = FileReaderService.ReadFile(FILE_PATH);
-            var uniqueWaysToWin = PlayCamelCardGames(stringInputs);
+            var uniqueWaysToWin = SumCamelCardHandsPlayed(stringInputs);
             MenuPrinterService.PrintSolution(uniqueWaysToWin.ToString());
         }
 
-        private int PlayCamelCardGames(List<string> stringInputs)
+        private int SumCamelCardHandsPlayed(List<string> stringInputs)
         {
             var totalWinnings = 0;
+            List<CamelCardGame> camelCardHands = ParseCamelCardGames(stringInputs);
+            for (int i = 1; i <= camelCardHands.Count; i++)
+            {
+                totalWinnings += camelCardHands[i - 1].Bid * i;
+            }
+
             return totalWinnings;
+        }
+
+        private List<CamelCardGame> ParseCamelCardGames(List<string> camelCardHands)
+        {
+            var camelCardGames = new List<CamelCardGame>();
+            foreach(var camelCardHand in camelCardHands)
+            {
+                var camelCardGame = GetCamelCardGameFromString(camelCardHand);
+                InsertIntoList(camelCardGames, camelCardGame);
+            }
+
+            return camelCardGames;
+        }
+
+        private CamelCardGame GetCamelCardGameFromString(string camelCardHand)
+        {
+            var camelCardGameParts = camelCardHand.Split(" ");
+            return new CamelCardGame
+            {
+                Hand = camelCardGameParts[0],
+                Bid = Int32.Parse(camelCardGameParts[1])
+            };
+        }
+
+        private void InsertIntoList(List<CamelCardGame> camelCardGames, CamelCardGame camelCardGame)
+        {
+            if (camelCardGames.Count == 0)
+            {
+                camelCardGames.Add(camelCardGame);
+                return;
+            }
+
+            var left = 0;
+            var right = camelCardGames.Count - 1;
+            while (left <= right)
+            {
+                var midPoint = left + (right - left) / 2;
+                var isBigger = camelCardGame.BiggerThan(camelCardGames[midPoint]);
+                if (isBigger)
+                {
+                    left = midPoint + 1;
+                }
+                else
+                {
+                    right = midPoint - 1;
+                }
+            }
+            camelCardGames.Insert(left, camelCardGame);
+        }
+
+        internal class CamelCardGame
+        {
+            public string Hand { get; set; }
+            public int Bid { get; set; }
+
+            public bool BiggerThan(CamelCardGame camelCardGame)
+            {
+                var currentHand = this.GetCardHand();
+                var compareHand = camelCardGame.GetCardHand();
+                if (currentHand > compareHand)
+                {
+                    return true;
+                }
+                else if(currentHand < compareHand)
+                {
+                    return false;
+                }
+                return CompareHand(camelCardGame);
+            }
+
+            private bool CompareHand(CamelCardGame camelCardGame)
+            {
+                var cardStrengthOrder = "23456789TJQKA";
+                for(int i = 0; i < Hand.Length; i++)
+                {
+                    var firstCharValue = cardStrengthOrder.IndexOf(Hand[i]);
+                    var secondCharValue = cardStrengthOrder.IndexOf(camelCardGame.Hand[i]);
+                    if (firstCharValue < secondCharValue) 
+                    { 
+                        return false; 
+                    }
+                    else if(firstCharValue > secondCharValue)
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+
+            private CardHand GetCardHand()
+            {
+                if (IsFiveOfAKind())
+                {
+                    return CardHand.FIVE_OF_A_KIND;
+                }
+                if (IsFourOfAKind())
+                {
+                    return CardHand.FOUR_OF_A_KIND;
+                }
+                if (IsFullHouse())
+                {
+                    return CardHand.FULL_HOUSE;
+                }
+                if (IsThreeOfAKind())
+                {
+                    return CardHand.THREE_OF_A_KIND;
+                }
+                if (IsTwoPairs())
+                {
+                    return CardHand.TWO_PAIRS;
+                }
+                if (IsOnePair())
+                {
+                    return CardHand.ONE_PAIR;
+                }
+
+                return CardHand.HIGH_CARD;
+            }
+
+            private bool IsFiveOfAKind()
+            {
+                var firstCard = Hand.ElementAt(0);
+                return Hand.All(c => c == firstCard);
+            }
+
+            private bool IsFourOfAKind()
+            {
+                var charCount = Hand
+                    .GroupBy(c => c)
+                    .Select(g => new { Char = g.Key, Count = g.Count() })
+                    .OrderByDescending(x => x.Count)
+                    .First();
+                
+                return charCount.Count == 4;
+            }
+
+            private bool IsFullHouse()
+            {
+                var charCount = Hand
+                    .GroupBy(c => c)
+                    .Select(g => new { Char = g.Key, Count = g.Count() })
+                    .OrderByDescending(x => x.Count)
+                    .ToList();
+
+                return charCount[0].Count == 3 && charCount[1].Count == 2;
+            }
+
+            private bool IsThreeOfAKind()
+            {
+                var charCount = Hand
+                    .GroupBy(c => c)
+                    .Select(g => new { Char = g.Key, Count = g.Count() })
+                    .OrderByDescending(x => x.Count)
+                    .First();
+
+                return charCount.Count == 3;
+            }
+
+            private bool IsTwoPairs()
+            {
+                var charCount = Hand
+                    .GroupBy(c => c)
+                    .Select(g => new { Char = g.Key, Count = g.Count() })
+                    .OrderByDescending(x => x.Count)
+                    .ToList();
+
+                return charCount[0].Count == 2 && charCount[1].Count == 2;
+            }
+
+            private bool IsOnePair()
+            {
+                var charCount = Hand
+                    .GroupBy(c => c)
+                    .Select(g => new { Char = g.Key, Count = g.Count() })
+                    .OrderByDescending(x => x.Count)
+                    .ToList();
+
+                return charCount[0].Count == 2 && charCount[1].Count == 1;
+            }
+        }
+
+        enum CardHand
+        {
+            HIGH_CARD = 0,
+            ONE_PAIR = 1,
+            TWO_PAIRS = 2,
+            THREE_OF_A_KIND = 3,
+            FULL_HOUSE = 4,
+            FOUR_OF_A_KIND = 5,
+            FIVE_OF_A_KIND = 6
         }
     }
 }
--- AdventOfCode2023/Solutions/Day_7/SolutionB.cs
+++ AdventOfCode2023/Solutions/Day_7/SolutionB.cs
@@ -6,9 +6,16 @@ using System.Collections.Generic;
 namespace AdventOfCode2023.Solutions.Day_7
 {
     /**
-     *  PART 2
-     *  TBD
-     **/
+    *  PART 2
+    *  1. Logic can remain mostly the same as Part 1
+    *  2. Parse every line of input into a CamelCardGame
+    *  3. A CamelCardGame should consist of a hand (string of length 5) and a bid (int)
+    *  4. Insert the CamelCardGame into a list of CamelCardGames such that the list is sorted by hand strength
+    *      4a. Need an algorithm to determine which hand is stronger. This will be used in the comparison.
+    *      4b. As our list of CamelCardGames is sorted during insertion, we can do a binary search through the current list to figure out the correct position
+    *  5. For each CamelCardGame in the list, calculate its winnings by multiplying its rank (index + 1) with the bid: (index + 1) * bid = winnings
+    *  6. Sum up all winnings
+    **/
     public class SolutionB : ISolution
     {
         private const string FILE_PATH = "Solutions/Day_7/7.in";
