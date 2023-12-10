655f62c48fbbe532c2a201b12910be66b2011463
Daniel Dutta <daniel.e.dutta@gmail.com>
2023-12-10 15:59:35 +0100

    - Added solution for day 10 part 1 - Added task description for day 10 part 2

--- AdventOfCode2023/Solutions/Day_10/SolutionA.cs
+++ AdventOfCode2023/Solutions/Day_10/SolutionA.cs
@@ -1,4 +1,7 @@
 ﻿using AdventOfCode2023.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AdventOfCode2023.Solutions.Day_10
 {
@@ -22,8 +25,114 @@ namespace AdventOfCode2023.Solutions.Day_10
 
         private int GetDistanceOfTileFurthestAwayFromStartingPoint()
         {
-            var distance = -1;
-            return distance;
+            var startingPoint = FindStartingPoint();
+            var connectedPipes = FindConnectedPipes(startingPoint);
+            var nextPipe = connectedPipes[0];
+            while(nextPipe.PipeShape != 'S') 
+            {
+                nextPipe = FindNextPipe(nextPipe);
+            }
+            startingPoint.PreviousPipe = nextPipe.PreviousPipe;
+
+            var previousPipe = startingPoint.PreviousPipe;
+            var currentDistance = 1;
+            var currentLargestDistance = -1;
+            while (previousPipe!.PipeShape != 'S')
+            {
+                previousPipe.DistanceFromStartingPoint = Math.Min(currentDistance, previousPipe.DistanceFromStartingPoint);
+                currentLargestDistance = Math.Max(currentLargestDistance, previousPipe.DistanceFromStartingPoint);
+                previousPipe = previousPipe.PreviousPipe;
+                currentDistance++;
+            }
+            return currentLargestDistance;
+        }
+
+        private Pipe FindStartingPoint()
+        {
+            var startingPoint = stringInputs.Where(s => s.Contains("S")).First();
+            var xCoordinates = startingPoint.IndexOf("S");
+            var yCoordinates = stringInputs.IndexOf(startingPoint);
+            var startingPointPipe = new Pipe
+            {
+                DistanceFromStartingPoint = 0,
+                PipeShape = 'S',
+                Coordinates = (xCoordinates, yCoordinates)
+            };
+
+            return startingPointPipe;
+        }
+
+        private List<Pipe> FindConnectedPipes(Pipe pipe)
+        {
+            var northTargetDirections = "|7FS";
+            var eastTargetDirections = "-J7S";
+            var southTargetDirections = "S|LJ";
+            var westTargetDirections = "-LFS";
+
+            var north = TryGetCharFromInput((pipe.Coordinates.Item1, pipe.Coordinates.Item2 - 1));
+            var east = TryGetCharFromInput((pipe.Coordinates.Item1 + 1, pipe.Coordinates.Item2));
+            var south = TryGetCharFromInput((pipe.Coordinates.Item1, pipe.Coordinates.Item2 + 1));
+            var west = TryGetCharFromInput((pipe.Coordinates.Item1 - 1, pipe.Coordinates.Item2));
+
+            var pipes = new List<Pipe>();
+            if (northTargetDirections.Contains(north) && southTargetDirections.Contains(pipe.PipeShape))
+            {
+                pipes.Add(CreateNewPipeFromPreviousPipe(pipe, (pipe.Coordinates.Item1, pipe.Coordinates.Item2 - 1), north));
+            }
+            if (eastTargetDirections.Contains(east) && westTargetDirections.Contains(pipe.PipeShape))
+            {
+                pipes.Add(CreateNewPipeFromPreviousPipe(pipe, (pipe.Coordinates.Item1 + 1, pipe.Coordinates.Item2), east));
+            }
+            if (southTargetDirections.Contains(south) && northTargetDirections.Contains(pipe.PipeShape))
+            {
+                pipes.Add(CreateNewPipeFromPreviousPipe(pipe, (pipe.Coordinates.Item1, pipe.Coordinates.Item2 + 1), south));
+            }
+            if (westTargetDirections.Contains(west) && eastTargetDirections.Contains(pipe.PipeShape))
+            {
+                pipes.Add(CreateNewPipeFromPreviousPipe(pipe, (pipe.Coordinates.Item1 - 1, pipe.Coordinates.Item2), west));
+            }
+
+            return pipes;
+        }
+
+        private Pipe CreateNewPipeFromPreviousPipe(Pipe previousPipe, (int, int) newCoordinates, char newPipeShape)
+        {
+            return new Pipe
+            {
+                Coordinates = newCoordinates,
+                PipeShape = newPipeShape,
+                DistanceFromStartingPoint = previousPipe.DistanceFromStartingPoint + 1,
+                PreviousPipe = previousPipe,
+            };
+        }
+
+        private char TryGetCharFromInput((int, int) coordinates)
+        {
+            var value = '.';
+            try
+            {
+                value = stringInputs[coordinates.Item2].ElementAt(coordinates.Item1);
+            }
+            catch (Exception ex) { }
+            return value;
+        }
+
+        private Pipe FindNextPipe(Pipe pipe)
+        {
+            var adjacentPipes = FindConnectedPipes(pipe);
+            var beforeCompareCoordinates = adjacentPipes.Where(p => p.Coordinates != pipe.PreviousPipe.Coordinates).ToList();
+            var nextPipe = adjacentPipes.First(p => p.Coordinates != pipe.PreviousPipe.Coordinates);
+            nextPipe.PreviousPipe = pipe;
+            return nextPipe;
+        }
+
+        internal class Pipe()
+        {
+            public (int, int) Coordinates { get; set; }
+            public char PipeShape { get; set; }
+            public int DistanceFromStartingPoint { get; set; }
+            public Pipe? PreviousPipe { get; set; }
+            public Pipe? NextPipe { get; set; }
         }
     }
 }
