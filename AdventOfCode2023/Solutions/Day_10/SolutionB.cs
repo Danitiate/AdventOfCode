828bd9747f3125a71bd145e71c3c344c01f2effe
Daniel Dutta <daniel.e.dutta@gmail.com>
2023-12-10 18:16:26 +0100

    - Added solution for Day 10 Part 2

--- AdventOfCode2023/Solutions/Day_10/SolutionB.cs
+++ AdventOfCode2023/Solutions/Day_10/SolutionB.cs
@@ -7,59 +7,230 @@ namespace AdventOfCode2023.Solutions.Day_10
 {
     /**
      *  PART 2
-     *  
+     *  1. Find the coordinates of every corner in the largest loop found in part 1
+     *  2. Consider every tile in the space between (LoopXMin, LoopYMin) to (LoopXMax, LoopYMax)
+     *  3. Perform the Even-Odd algorithm on every tile in this space (https://en.wikipedia.org/wiki/Even%E2%80%93odd_rule)
+     *  4. If there are an odd number of edges crossed, the tile is inside the loop
+     *  5. Sum amount of odd numbered edges crossed
      **/
     public class SolutionB : Solution
     {
         protected override string GetSolutionOutput()
         {
-            var amountOfStepsRequired = GetSumOfExtrapolatedNumbers();
-            return amountOfStepsRequired.ToString();
+            var amountOfTilesEnclosedInLoop = GetAmountOfTilesEnclosedInLoop();
+            return amountOfTilesEnclosedInLoop.ToString();
         }
 
-        private int GetSumOfExtrapolatedNumbers()
+        private int GetAmountOfTilesEnclosedInLoop()
         {
-            var sum = 0;
-            var numbersList = GetNumbersFromInput();
-            foreach (var numbers in numbersList)
+            var amountOfTilesEnclosedInLoop = 0;
+            var cornersInLoop = GetCornersInLoop();
+            var loopXMin = cornersInLoop.MinBy(xy => xy.Item1).Item1;
+            var loopXMax = cornersInLoop.MaxBy(xy => xy.Item1).Item1;
+            var loopYMin = cornersInLoop.MinBy(xy => xy.Item2).Item2;
+            var loopYMax = cornersInLoop.MaxBy(xy => xy.Item2).Item2;
+
+            for (int i = loopYMin; i < loopYMax; i++)
             {
-                sum += GetExtrapolatedNumberFromList(numbers);
+                for(int j = loopXMin; j < loopXMax; j++)
+                {
+                    var tile = stringInputs[i].ElementAt(j);
+                    if (TileIsInsideOfLoop((j, i), cornersInLoop))
+                    {
+                        amountOfTilesEnclosedInLoop++;
+                    } 
+                }
             }
-            return sum;
+
+            return amountOfTilesEnclosedInLoop;
         }
 
-        private List<List<int>> GetNumbersFromInput()
+        private List<(int, int)> GetCornersInLoop()
         {
-            var numbers = new List<List<int>>();
-            foreach (var stringInput in stringInputs)
+            var cornerShapes = "LJ7FS";
+            List<(int, int)> points = new List<(int, int)>();
+            var loop = GetLoop();
+            points.Add(loop.Coordinates);
+            var previousPipe = loop.PreviousPipe;
+            while (previousPipe!.PipeShape != 'S')
             {
-                var numbersString = stringInput.Split(" ");
-                numbers.Add(numbersString.Select(n => Int32.Parse(n)).ToList());
+                if (cornerShapes.Contains(previousPipe!.PipeShape))
+                {
+                    points.Add((previousPipe.Coordinates));
+                }
+                previousPipe = previousPipe.PreviousPipe;
             }
 
-            return numbers;
+            return points;
         }
 
-        private int GetExtrapolatedNumberFromList(List<int> numbers)
+        private Pipe GetLoop()
         {
-            if (numbers.Count == 0 || numbers.All(n => n == 0))
+            var startingPoint = FindStartingPoint();
+            var connectedPipes = FindConnectedPipes(startingPoint);
+            var nextPipe = connectedPipes[0];
+            while (nextPipe.PipeShape != 'S')
             {
-                return 0;
+                nextPipe = FindNextPipe(nextPipe);
             }
-
-            var numbersDifferenceList = GetDifferencesFromNumbers(numbers);
-            return numbers.First() - GetExtrapolatedNumberFromList(numbersDifferenceList);
+            startingPoint.PreviousPipe = nextPipe.PreviousPipe;
+            return startingPoint;
         }
 
-        private List<int> GetDifferencesFromNumbers(List<int> numbers)
+        private Pipe FindStartingPoint()
         {
-            var numbersDifferenceList = new List<int>();
-            for (int i = 1; i < numbers.Count; i++)
+            var startingPoint = stringInputs.Where(s => s.Contains("S")).First();
+            var xCoordinates = startingPoint.IndexOf("S");
+            var yCoordinates = stringInputs.IndexOf(startingPoint);
+            var startingPointPipe = new Pipe
             {
-                numbersDifferenceList.Add(numbers[i] - numbers[i - 1]);
+                DistanceFromStartingPoint = 0,
+                PipeShape = 'S',
+                Coordinates = (xCoordinates, yCoordinates)
+            };
+
+            return startingPointPipe;
+        }
+
+        private List<Pipe> FindConnectedPipes(Pipe pipe)
+        {
+            var northTargetDirections = "|7FS";
+            var eastTargetDirections = "-J7S";
+            var southTargetDirections = "S|LJ";
+            var westTargetDirections = "-LFS";
+
+            var north = TryGetCharFromInput((pipe.Coordinates.Item1, pipe.Coordinates.Item2 - 1));
+            var east = TryGetCharFromInput((pipe.Coordinates.Item1 + 1, pipe.Coordinates.Item2));
+            var south = TryGetCharFromInput((pipe.Coordinates.Item1, pipe.Coordinates.Item2 + 1));
+            var west = TryGetCharFromInput((pipe.Coordinates.Item1 - 1, pipe.Coordinates.Item2));
+
+            var pipes = new List<Pipe>();
+            if (northTargetDirections.Contains(north) && southTargetDirections.Contains(pipe.PipeShape))
+            {
+                pipes.Add(CreateNewPipeFromPreviousPipe(pipe, (pipe.Coordinates.Item1, pipe.Coordinates.Item2 - 1), north));
+            }
+            if (eastTargetDirections.Contains(east) && westTargetDirections.Contains(pipe.PipeShape))
+            {
+                pipes.Add(CreateNewPipeFromPreviousPipe(pipe, (pipe.Coordinates.Item1 + 1, pipe.Coordinates.Item2), east));
+            }
+            if (southTargetDirections.Contains(south) && northTargetDirections.Contains(pipe.PipeShape))
+            {
+                pipes.Add(CreateNewPipeFromPreviousPipe(pipe, (pipe.Coordinates.Item1, pipe.Coordinates.Item2 + 1), south));
+            }
+            if (westTargetDirections.Contains(west) && eastTargetDirections.Contains(pipe.PipeShape))
+            {
+                pipes.Add(CreateNewPipeFromPreviousPipe(pipe, (pipe.Coordinates.Item1 - 1, pipe.Coordinates.Item2), west));
             }
 
-            return numbersDifferenceList;
+            return pipes;
+        }
+
+        private Pipe CreateNewPipeFromPreviousPipe(Pipe previousPipe, (int, int) newCoordinates, char newPipeShape)
+        {
+            return new Pipe
+            {
+                Coordinates = newCoordinates,
+                PipeShape = newPipeShape,
+                DistanceFromStartingPoint = previousPipe.DistanceFromStartingPoint + 1,
+                PreviousPipe = previousPipe,
+            };
+        }
+
+        private char TryGetCharFromInput((int, int) coordinates)
+        {
+            var value = '.';
+            try
+            {
+                value = stringInputs[coordinates.Item2].ElementAt(coordinates.Item1);
+            }
+            catch (Exception ex) { }
+            return value;
+        }
+
+        private Pipe FindNextPipe(Pipe pipe)
+        {
+            var adjacentPipes = FindConnectedPipes(pipe);
+            var beforeCompareCoordinates = adjacentPipes.Where(p => p.Coordinates != pipe.PreviousPipe.Coordinates).ToList();
+            var nextPipe = adjacentPipes.First(p => p.Coordinates != pipe.PreviousPipe.Coordinates);
+            nextPipe.PreviousPipe = pipe;
+            return nextPipe;
+        }
+
+        private bool TileIsInsideOfLoop((int, int) coordinates, List<(int, int)> cornersInLoop)
+        {
+            var tileIsInsideOfPolygon = false;
+            for (int i = 0, j = cornersInLoop.Count - 1; i < cornersInLoop.Count; j = i++)
+            {
+                var x0y0 = cornersInLoop[i];
+                var x1y1 = cornersInLoop[j];
+                if (CoordinatesIsOnACorner(coordinates, x0y0))
+                {
+                    return false;
+                }
+                if (CoordinatesIsVerticallyBetweenTwoPoints(coordinates.Item2, x0y0.Item2, x1y1.Item2))
+                {
+                    var deltaX = x1y1.Item1 - x0y0.Item1;
+                    var deltaY = x1y1.Item2 - x0y0.Item2;
+                    var deltaPX = coordinates.Item1 - x0y0.Item1;
+                    var deltaPY = coordinates.Item2 - x0y0.Item2;
+                    var slope = (deltaPX * deltaY) - (deltaX * deltaPY);
+                    if (CoordinatesIsOnAnEdge(slope))
+                    {
+                        return false;
+                    }
+                    else if(CoordinatesIsToTheLeftOfVector(slope) != VectorIsMovingDownwards(deltaY))
+                    {
+                        tileIsInsideOfPolygon = !tileIsInsideOfPolygon;
+                    }
+                }
+                else if (CoordinatesIsBetweenAlignedVerticalPoints(coordinates, x0y0, x1y1))
+                {
+                    return false;
+                }
+            }
+
+            return tileIsInsideOfPolygon;
+        }
+
+        private bool CoordinatesIsOnACorner((int, int) coordinates, (int, int) x0y0)
+        {
+            return coordinates == x0y0;
+        }
+
+        private bool CoordinatesIsVerticallyBetweenTwoPoints(int coordinatesY, int y0, int y1)
+        {
+            return y0 > coordinatesY != y1 > coordinatesY;
+        }
+
+        private bool CoordinatesIsOnAnEdge(int slope)
+        {
+            return slope == 0;
+        }
+
+        private bool CoordinatesIsToTheLeftOfVector(int slope)
+        {
+            return slope < 0;
+        }
+
+        private bool VectorIsMovingDownwards(int deltaY)
+        {
+            return deltaY < 0;
+        }
+
+        private bool CoordinatesIsBetweenAlignedVerticalPoints((int, int) coordinates, (int, int) x0y0, (int, int) x1y1)
+        {
+            var coordinatesIsOnTheSameLine = x0y0.Item2 == coordinates.Item2 && x1y1.Item2 == coordinates.Item2;
+            var xCoordinatesIsBetweenTwoPoints = (x0y0.Item1 > coordinates.Item1) != (x1y1.Item1 > coordinates.Item1);
+            return coordinatesIsOnTheSameLine && xCoordinatesIsBetweenTwoPoints;
+        }
+
+        internal class Pipe()
+        {
+            public (int, int) Coordinates { get; set; }
+            public char PipeShape { get; set; }
+            public int DistanceFromStartingPoint { get; set; }
+            public Pipe? PreviousPipe { get; set; }
+            public Pipe? NextPipe { get; set; }
         }
     }
 }
