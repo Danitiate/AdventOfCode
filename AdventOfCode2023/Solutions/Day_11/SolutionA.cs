b0c6d84f801f531f8b68138c3ac07dfeecd58099
Daniel Dutta <daniel.e.dutta@gmail.com>
2023-12-11 08:36:23 +0100

    - Added solution to day 11 part 1 - Added day 11 part 2 task description

--- AdventOfCode2023/Solutions/Day_11/SolutionA.cs
+++ AdventOfCode2023/Solutions/Day_11/SolutionA.cs
@@ -1,4 +1,7 @@
 ﻿using AdventOfCode2023.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AdventOfCode2023.Solutions.Day_11
 {
@@ -22,7 +25,89 @@ namespace AdventOfCode2023.Solutions.Day_11
 
         private int GetSumOfShortestPathsBetweenGalaxies()
         {
+            var galaxies = GetGalaxiesFromInput();
+            var sum = calculateDistanceOfAllGalaxies(galaxies);
+            return sum;
+        }
+
+        private List<(int, int)> GetGalaxiesFromInput()
+        {
+            var universe = GetExpandedUniverseFromInput();
+            var galaxies = GetGalaxyCoordinatesInUniverse(universe);
+            return galaxies;
+        }
+
+        private List<string> GetExpandedUniverseFromInput()
+        {
+            var universe = new List<string>();
+            foreach (var stringInput in stringInputs)
+            {
+                universe.Add(stringInput);
+                if (stringInput.All(s => s == '.'))
+                {
+                    universe.Add(stringInput);
+                }
+            }
+
+            var universeWidth = universe[0].Length;
+            for(int i = 0; i < universeWidth; i++)
+            {
+                var columnIsEmptySpace = true;
+                for(int j = 0; j < universe.Count(); j++)
+                {
+                    var nextChar = universe[j].ElementAt(i);
+                    if (nextChar != '.')
+                    {
+                        columnIsEmptySpace = false;
+                        break;
+                    }
+                }
+                if (columnIsEmptySpace)
+                {
+                    for (int j = 0; j < universe.Count(); j++)
+                    {
+                        universe[j] = universe[j].Insert(i, ".");
+                    }
+                    i++;
+                    universeWidth++;
+                }
+            }
+
+            return universe;
+        }
+
+        private List<(int, int)> GetGalaxyCoordinatesInUniverse(List<string> universe)
+        {
+            var galaxies = new List<(int, int)>();
+            for (int i = 0; i < universe.Count; i++)
+            {
+                var universeInput = universe[i];
+                for(int j = 0; j < universeInput.Length; j++)
+                {
+                    if (universeInput.ElementAt(j) == '#')
+                    {
+                        galaxies.Add((j, i));
+                    }
+                }
+            }
+
+            return galaxies;
+        }
+
+        private int calculateDistanceOfAllGalaxies(List<(int, int)> galaxies)
+        {
             var sum = 0;
+            for (int i = 0; i < galaxies.Count - 1; i++)
+            {
+                for(int j = i + 1; j < galaxies.Count; j++)
+                {
+                    var galaxyA = galaxies[i];
+                    var galaxyB = galaxies[j];
+                    sum += Math.Abs(galaxyB.Item1 - galaxyA.Item1);
+                    sum += Math.Abs(galaxyB.Item2 - galaxyA.Item2);
+                }
+            }
+
             return sum;
         }
     }
--- AdventOfCode2023/Solutions/Day_11/SolutionB.cs
+++ AdventOfCode2023/Solutions/Day_11/SolutionB.cs
@@ -4,7 +4,9 @@ namespace AdventOfCode2023.Solutions.Day_11
 {
     /**
      *  PART 2
-     *  1. 
+     *  1. Keep same solution as part 1
+     *  2. Instead of adding new columns or rows to the expanded universe list, replace the empty row / column with a row of "E" for empty.
+     *  3. Then in the calculations, count distance like normal, but for every "E" that is passed, add 1.000.000
      **/
     public class SolutionB : Solution
     {
