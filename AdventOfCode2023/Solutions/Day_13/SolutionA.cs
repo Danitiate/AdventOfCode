9e180d4f2b6ebc5f537e0c76a2fb0c50bd372419
Daniel Dutta <daniel.e.dutta@gmail.com>
2023-12-13 21:28:16 +0100

    - Setup Day 13 solution - Added day 13 task description

---
+++ AdventOfCode2023/Solutions/Day_13/SolutionA.cs
@@ -0,0 +1,26 @@
+﻿using AdventOfCode2023.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2023.Solutions.Day_13
+{
+    /**
+     *  PART 1
+     *  1.
+     **/
+    public class SolutionA : Solution
+    {
+        protected override string GetSolutionOutput()
+        {
+            var sumOfAllNotes = GetSumOfAllNotes();
+            return sumOfAllNotes.ToString();
+        }
+
+        private int GetSumOfAllNotes()
+        {
+            var sum = 0;
+            return sum;
+        }
+    }
+}
---
+++ AdventOfCode2023/Solutions/Day_13/SolutionB.cs
@@ -0,0 +1,26 @@
+﻿using AdventOfCode2023.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace AdventOfCode2023.Solutions.Day_13
+{
+    /**
+     *  PART 2
+     *  1.
+     **/
+    public class SolutionB : Solution
+    {
+        protected override string GetSolutionOutput()
+        {
+            var sumOfAllNotes = GetSumOfAllNotes();
+            return sumOfAllNotes.ToString();
+        }
+
+        private int GetSumOfAllNotes()
+        {
+            var sum = 0;
+            return sum;
+        }
+    }
+}
