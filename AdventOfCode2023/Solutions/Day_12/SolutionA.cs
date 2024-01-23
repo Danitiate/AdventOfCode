049fc633820341da77447ed423c84c41fefc8cfb
Daniel Dutta <daniel.e.dutta@gmail.com>
2024-01-23 15:57:14 +0100

    - Added solution to Day 12 Part 1

--- AdventOfCode2023/Solutions/Day_12/SolutionA.cs
+++ AdventOfCode2023/Solutions/Day_12/SolutionA.cs
@@ -2,12 +2,18 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace AdventOfCode2023.Solutions.Day_12
 {
     /**
      *  PART 1
-     *  1.
+     *  1. Parse input into two categories:
+     *      A: The spring conditions
+     *      B: A list of group sizes
+     *  2. Find every possible spring condition rows by replacing any "?" value with "." and "#" as two new instances
+     *  3. Recursively iterate through all possible spring conditions and count all results with a valid arrangement
+     *  4. Sum the result of every row iterated through step 3
      **/
     public class SolutionA : Solution
     {
@@ -19,8 +25,113 @@ namespace AdventOfCode2023.Solutions.Day_12
 
         private int GetSumOfPossibleArrangements()
         {
+            var springRows = ParseInputToSpringRows();
             var sum = 0;
+            foreach(var springRow in springRows)
+            {
+                sum += CountNumberOfPossibleArrangementsForSpringRow(springRow);
+            }
             return sum;
         }
+
+        private List<SpringRow> ParseInputToSpringRows()
+        {
+            var springRows = new List<SpringRow>();
+            foreach(var stringInput in stringInputs)
+            {
+                var splitStringInput = stringInput.Split(" ");
+                springRows.Add(new SpringRow
+                {
+                    SpringConditions = splitStringInput[0],
+                    GroupSizes = splitStringInput[1].Split(",")
+                        .Select(s => Int32.Parse(s))
+                        .ToList()
+                });
+            }
+
+            return springRows;
+        }
+
+        private int CountNumberOfPossibleArrangementsForSpringRow(SpringRow springRow)
+        {
+            if (!HasEnoughDefectsAndWildcardsToMatchGroup(springRow))
+            {
+                return 0;
+            }
+
+            var amountOfPossibleArrangements = 0;
+            if (IsPredictionComplete(springRow))
+            {
+                if (IsArrangementPossible(springRow))
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 0;
+                }
+            } 
+            else
+            {
+                var regex = new Regex("\\?");
+                var currentSpringConditionString = springRow.SpringConditions;
+                var springRowOk = new SpringRow
+                {
+                    GroupSizes = springRow.GroupSizes,
+                    SpringConditions = regex.Replace(currentSpringConditionString, ".", 1)
+                };
+
+                var springRowDefect = new SpringRow
+                {
+                    GroupSizes = springRow.GroupSizes,
+                    SpringConditions = regex.Replace(currentSpringConditionString, "#", 1)
+                };
+
+                amountOfPossibleArrangements += CountNumberOfPossibleArrangementsForSpringRow(springRowOk);
+                amountOfPossibleArrangements += CountNumberOfPossibleArrangementsForSpringRow(springRowDefect);
+            }
+
+            return amountOfPossibleArrangements;
+        }
+
+        private bool HasEnoughDefectsAndWildcardsToMatchGroup(SpringRow springRow)
+        {
+            var expectedAmountOfDefectsAndWildcards = springRow.GroupSizes.Sum();
+            var amountOfDefects = springRow.SpringConditions.Count(s => s == '#');
+            var amountOfWildcards = springRow.SpringConditions.Count(s => s == '?');
+            return amountOfDefects + amountOfWildcards >= expectedAmountOfDefectsAndWildcards && amountOfDefects <= expectedAmountOfDefectsAndWildcards;
+        }
+
+        private bool IsPredictionComplete(SpringRow springRow)
+        {
+            return springRow.SpringConditions.All(c => c != '?');
+        }
+
+        private bool IsArrangementPossible(SpringRow springRow)
+        {
+            var springGroups = springRow.SpringConditions.Split(".", StringSplitOptions.RemoveEmptyEntries);
+            if (springGroups.Length == springRow.GroupSizes.Count)
+            {
+                for(int i = 0; i < springGroups.Length; i++)
+                {
+                    if (springGroups[i].Length != springRow.GroupSizes[i])
+                    {
+                        return false;
+                    }
+                }
+            }
+            else
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+
+    internal class SpringRow()
+    {
+        public string SpringConditions { get; set; }
+        public List<int> GroupSizes { get; set; }
     }
 }
---
+++ AdventOfCode2023Tests/Day12Tests.cs
@@ -0,0 +1,27 @@
+using AdventOfCode2023.Solutions.Day_12;
+
+namespace AdventOfCode2023Tests
+{
+    public class Day12Tests
+    {
+        List<string> testStringInput = new List<string>
+        {
+            "???.### 1,1,3",
+            ".??..??...?##. 1,1,3",
+            "?#?#?#?#?#?#?#? 1,3,1,6",
+            "????.#...#... 4,1,1",
+            "????.######..#####. 1,6,5",
+            "?###???????? 3,2,1"
+        };
+
+        [Fact]
+        public void SolutionA()
+        {
+            var sut = new SolutionA();
+
+            var result = sut.TestSolve(testStringInput);
+
+            Assert.Equal("21", result);
+        }
+    }
+}
