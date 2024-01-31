a6922227d1a24b7c17a3ae4964db9e27f39b4a77
Daniel Dutta <daniel.e.dutta@gmail.com>
2024-01-31 13:28:03 +0100

    - Added solution for Day 16 Part 1

---
+++ AdventOfCode2023/Solutions/Day_16/Cell.cs
@@ -0,0 +1,11 @@
+﻿namespace AdventOfCode2023.Solutions.Day_16
+{
+    public class Cell
+    {
+        public int X {  get; set; }
+        public int Y { get; set; }
+        public char Tile { get; set; }
+        public bool Energized { get; set; } = false;
+        public Direction PreviousDirections { get; set; } = Direction.UNKNOWN;
+    }
+}
--- AdventOfCode2023/Solutions/Day_16/SolutionA.cs
+++ AdventOfCode2023/Solutions/Day_16/SolutionA.cs
@@ -1,11 +1,17 @@
 ﻿using AdventOfCode2023.Models;
-using System;
+using System.Linq;
 
 namespace AdventOfCode2023.Solutions.Day_16
 {
     /**
      *  PART 1
-     * 
+     *  1. Parse input into a grid of cells
+     *  2. Starting from the north-west corner (0,0), traverse eastwards and mark each cell along the way
+     *      2a. If we encounter a '.', continue the same direction
+     *      2b. If we encounter a '-', split the direction if the current direction is north or south. Otherwise, continue the same direction
+     *      2c. If we encounter a '|', split the direction if the current direction is west or east. Otherwise, continue the same direction
+     *      2d. There is no need to continue traversing if we are entering a cell we have already entered before from the same direction.
+     *  3. After all cells are traversed and marked, count the amount of energized cells and return the results
      **/
     public class SolutionA : Solution
     {
@@ -17,7 +23,127 @@ namespace AdventOfCode2023.Solutions.Day_16
 
         private int CountEnergizedTiles()
         {
-            throw new NotImplementedException();
+            var grid = ParseInputIntoGrid();
+            TraverseGrid(grid, grid[0,0], Direction.EAST);
+            var amountOfEnergizedTiles = 0;
+            foreach (var cell in grid)
+            {
+                if (cell.Energized)
+                {
+                    amountOfEnergizedTiles++;
+                }
+            }
+
+            return amountOfEnergizedTiles;
+        }
+
+        private Cell[,] ParseInputIntoGrid()
+        {
+            var firstRow = stringInputs[0];
+            var grid = new Cell[firstRow.Length, stringInputs.Count];
+            for (int i = 0; i < stringInputs.Count; i++)
+            {
+                var currentRow = stringInputs[i];
+                for (int j = 0; j < currentRow.Length; j++)
+                {
+                    var newCell = new Cell
+                    {
+                        X = j,
+                        Y = i,
+                        Tile = currentRow.ElementAt(j)
+                    };
+
+                    grid[j, i] = newCell;
+                }
+            }
+
+            return grid;
+        }
+
+        private void TraverseGrid(Cell[,] grid, Cell currentCell, Direction currentDirection)
+        {
+            currentCell.Energized = true;
+            if (currentCell.PreviousDirections.HasFlag(currentDirection))
+            {
+                return;
+            }
+            currentCell.PreviousDirections |= currentDirection;
+
+            currentDirection = DetermineNextDirection(currentDirection, currentCell);
+            if (currentDirection.HasFlag(Direction.NORTH) && currentCell.Y > 0)
+            {
+                TraverseGrid(grid, grid[currentCell.X, currentCell.Y - 1], Direction.NORTH);
+            }
+
+            if (currentDirection.HasFlag(Direction.EAST) && currentCell.X < grid.GetLength(0) - 1)
+            {
+                TraverseGrid(grid, grid[currentCell.X + 1, currentCell.Y], Direction.EAST);
+            }
+
+            if (currentDirection.HasFlag(Direction.SOUTH) && currentCell.Y < grid.GetLength(1) - 1)
+            {
+                TraverseGrid(grid, grid[currentCell.X, currentCell.Y + 1], Direction.SOUTH);
+            }
+
+            if (currentDirection.HasFlag(Direction.WEST) && currentCell.X > 0)
+            {
+                TraverseGrid(grid, grid[currentCell.X - 1, currentCell.Y], Direction.WEST);
+            }
+        }
+
+        private Direction DetermineNextDirection(Direction currentDirection, Cell currentCell)
+        {
+            if (currentCell.Tile == '|' && (currentDirection == Direction.EAST || currentDirection == Direction.WEST)) 
+            {
+                return Direction.NORTH | Direction.SOUTH;
+            }
+
+            if (currentCell.Tile == '-' && (currentDirection == Direction.NORTH || currentDirection == Direction.SOUTH))
+            {
+                return Direction.EAST | Direction.WEST;
+            }
+
+            if (currentCell.Tile == '/')
+            {
+                if (currentDirection == Direction.NORTH)
+                {
+                    return Direction.EAST;
+                }
+                if (currentDirection == Direction.EAST)
+                {
+                    return Direction.NORTH;
+                }
+                if (currentDirection == Direction.SOUTH)
+                {
+                    return Direction.WEST;
+                }
+                if (currentDirection == Direction.WEST)
+                {
+                    return Direction.SOUTH;
+                }
+            }
+
+            if (currentCell.Tile == '\\')
+            {
+                if (currentDirection == Direction.NORTH)
+                {
+                    return Direction.WEST;
+                }
+                if (currentDirection == Direction.WEST)
+                {
+                    return Direction.NORTH;
+                }
+                if (currentDirection == Direction.SOUTH)
+                {
+                    return Direction.EAST;
+                }
+                if (currentDirection == Direction.EAST)
+                {
+                    return Direction.SOUTH;
+                }
+            }
+
+            return currentDirection;
         }
     }
 }
